4e9debffa9c92d90594fa357e5c88d2353595778
Ethiraj K <ethirajit@gmail.com>
2024-04-06 19:03:29 +0530

    Enquery form backend support

--- voicebank/www/enquery.py
+++ voicebank/www/enquery.py
@@ -6,20 +6,20 @@ from datetime import datetime, timedelta
 def get_context(context):
     context.no_cache = 1
     if frappe.form_dict.member_id_list:
-        name = sanitize_html(frappe.form_dict.get('name'))
+        cust_name = sanitize_html(frappe.form_dict.get('cust_name'))
         email_id = sanitize_html(frappe.form_dict.get('email_id'))
         phone_number = sanitize_html(frappe.form_dict.get('phone_number'))
         member_id_list = sanitize_html(frappe.form_dict.get('member_id_list'))
-        context.results = search(name, email_id, phone_number, member_id_list)
+        context.results = search(cust_name, email_id, phone_number, member_id_list)
         context.update(context.results)
     else:
         context.title = _("Search")
 
 @frappe.whitelist(allow_guest=True)
-def search(name, email_id, phone_number, member_id_list):
+def search(cust_name, email_id, phone_number, member_id_list):
 
     emquery_filters_org = {
-        "name": name,
+        "cust_name": cust_name,
         "email_id": email_id,
         "phone_number": phone_number,
         "member_id_list": member_id_list
@@ -28,7 +28,6 @@ def search(name, email_id, phone_number, member_id_list):
     
     results = {}
     print ("Original Value of filter")
-    print ( "voice_bank_filters_org: ", emquery_filters_org)
-
+    print ( "enquery_form_org: ", emquery_filters_org)
 
     return { "results" : results }
